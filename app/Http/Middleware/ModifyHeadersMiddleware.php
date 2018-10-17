143d9ac57e7a54ad0d41d87924840c70662c7055
Hayden Bruin <bruinhayden@gmail.com>
2018-10-18 00:09:11 +1300

    Updating middleware to support creds

--- app/Http/Middleware/ModifyHeadersMiddleware.php
+++ app/Http/Middleware/ModifyHeadersMiddleware.php
@@ -16,8 +16,9 @@ class ModifyHeadersMiddleware
     public function handle( $request, Closure $next )
     {
         $response = $next( $request );
-        $response->header( 'Access-Control-Allow-Origin', 'http://localhost:3000' );
+        $response->header( 'Access-Control-Allow-Origin', 'http://127.0.0.1:3000' );
         $response->header( 'Access-Control-Allow-Headers', 'Origin, Content-Type' );
+        $response->header( 'Access-Control-Allow-Credentials', 'true');
 
         return $response;
     }
