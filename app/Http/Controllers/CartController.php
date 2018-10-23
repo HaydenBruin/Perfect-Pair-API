de6019b456c80531d2e1ca579e998a3e2b644c13
Hayden Bruin <bruinhayden@gmail.com>
2018-10-23 19:24:31 +1300

    sending through quantity totals via cart api

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -16,7 +16,8 @@ class CartController extends Controller
         $cart = array(
             'overview' => [
                 'totalPrice' => 0.00,
-                'totalSavings' => 0.00
+                'totalSavings' => 0.00,
+                'totalFullPrice' => 0.00
             ],
             'products' => []
         );
@@ -35,16 +36,22 @@ class CartController extends Controller
                 $product['description'] = $getProduct->description;
                 $product['price'] = number_format($getProduct->price,2);
                 $product['saleprice'] = number_format($getProduct->saleprice,2);
+                $product['totalprice'] = number_format($getProduct->price * $cartProduct['quantity'],2);
+                $product['totalsaleprice'] = number_format($getProduct->saleprice * $cartProduct['quantity'],2);
                 $product['inventory'] = $getProduct->inventory;
                 $product['slug'] = $getProduct->slug;
                 $cart['products'][] = $product;
 
                 // UPDATE TOTAL PRICING
-                $cart['overview']['totalPrice'] += $getProduct->saleprice ? $getProduct->saleprice : $getProduct->price;
-                $cart['overview']['totalSavings'] += $getProduct->price - ($getProduct->saleprice ? $getProduct->saleprice : $getProduct->price);
+                for($i = 0; $i < $cartProduct['quantity']; $i++)
+                {
+                    $cart['overview']['totalPrice'] += $getProduct->saleprice ? $getProduct->saleprice : $getProduct->price;
+                    $cart['overview']['totalSavings'] += $getProduct->price - ($getProduct->saleprice ? $getProduct->saleprice : $getProduct->price);
+                }
             }
             $cart['overview']['totalPrice'] = number_format($cart['overview']['totalPrice'], 2);
             $cart['overview']['totalSavings'] = number_format($cart['overview']['totalSavings'], 2);
+            $cart['overview']['totalFullPrice'] = number_format($cart['overview']['totalSavings'] + $cart['overview']['totalPrice'], 2);
         }
 
         return response()->json([
