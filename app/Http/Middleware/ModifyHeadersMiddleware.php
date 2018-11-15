401ff8aa181adcef1740a1d7a28c975d7347c064
Hayden Bruin <bruinhayden@gmail.com>
2018-11-15 21:24:25 +1300

    Adding in cors middleware plug

--- app/Http/Middleware/ModifyHeadersMiddleware.php
+++ app/Http/Middleware/ModifyHeadersMiddleware.php
@@ -16,7 +16,11 @@ class ModifyHeadersMiddleware
     public function handle( $request, Closure $next )
     {
         $response = $next( $request );
-        $response->header( 'Access-Control-Allow-Origin', 'https://perfectpair.nz' );
+        echo "<pre>";
+        print_r($response);
+        //$response->header( 'Access-Control-Allow-Origin', 'http://perfectpair.nz' );
+        //kkk
+        $response->header( 'Access-Control-Allow-Origin', 'http://127.0.0.1:3000' );
         $response->header( 'Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT' );
         $response->header( 'Access-Control-Allow-Headers', 'Origin, Content-Type' );
         $response->header( 'Access-Control-Allow-Credentials', 'true');
